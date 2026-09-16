5d53af85b80ef7c397f2632786929d91e1834ef3
dreamsnama2021-git <dreamsnama2021@gmail.com>
2026-09-16 17:22:29 +0530

    Delete src directory

--- src/routes/_authenticated/admin/products.$id.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/admin/products/$id")({
-  beforeLoad: () => { throw redirect({ to: "/admin/products" }); },
-  component: () => null,
-});
